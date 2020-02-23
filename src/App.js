d94863652adfe8f7f186a20ae225ffd6eea41d48
pokkrong <pokkrong@dotpropertygroup.com>
2020-02-23 16:43:18 +0700

    updating our app with a shop page.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom'
 import './App.css'
 import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = (props) => {
-  // console.log('hats props', props)
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  )}
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={Homepage}/>
-      <Route exact path='/shop/hats' component={HatsPage}/>
+      <Route exact path='/shop' component={ShopPage}/>
     </Switch>
   );
 }
