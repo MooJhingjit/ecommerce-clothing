d813bee98238a0382e26795f70d4d6e4822fada8
pokkrong <pokkrong@dotpropertygroup.com>
2020-02-24 20:14:55 +0700

    created sign form

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+  
+  handleSubmit = event => {
+    event.preventDfault();
+    this.setState({email: '', password: ''})
+  }
+
+  handleChange = event => {
+    let { value, name } = event.target;
+
+    this.setState({[name]: value});
+  }
+
+  render () {
+    return (
+      <div className="sign-in">
+        <h2 class="title">I already have an account</h2>
+        <span class="title">Sign in with your email and password</span>
+
+        <form onSubmit={() => this.handleSubmit()}>
+          <FormInput type="email" name="email" label="email" value={this.state.email} handleChange={this.handleChange} required/>
+          <FormInput type="password" name="password" label="password" value={this.state.password} handleChange={this.handleChange} required/>
+
+          <CustomButton type="submit">Submit Form</CustomButton>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignIn;
