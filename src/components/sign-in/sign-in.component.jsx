e2c270a279fec84ee0beba28f0825931435ab2c7
pokkrong <pokkrong@dotpropertygroup.com>
2020-03-05 23:14:35 +0700

    Before setting the redux

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -13,9 +13,16 @@ class SignIn extends React.Component{
     };
   }
   
-  handleSubmit = event => {
-    event.preventDfault();
-    this.setState({email: '', password: ''})
+  handleSubmit = async event => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    console.log('sign in ', this.state);
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({email: '', password: ''})
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   handleChange = event => {
@@ -34,7 +41,7 @@ class SignIn extends React.Component{
           <FormInput type="email" name="email" label="email" value={this.state.email} handleChange={this.handleChange} required/>
           <FormInput type="password" name="password" label="password" value={this.state.password} handleChange={this.handleChange} required/>
           <div className="buttons">
-            <CustomButton type="submit">Submit Form</CustomButton>
+            <CustomButton type="submit">SIGN IN</CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>SIGN IN WITH GOOGLE</CustomButton>
           </div>
           
