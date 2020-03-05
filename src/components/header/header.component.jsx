298a90b2248b7bc3374d9ba3bedc6d0c3ca68962
pokkrong <pokkrong@dotpropertygroup.com>
2020-03-06 00:03:14 +0700

    Added redux, and implemented userReducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
 import './header.styles.scss'
 
 const Header = ({currentUser}) => {
+  console.log('currentUser', currentUser);
   return (
     <div className="header">
       <Link className='logo-comtainer' to='/'>
@@ -24,4 +26,8 @@ const Header = ({currentUser}) => {
   )
 }
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
